a80704449c8d3b30a048a3fdadc2dde7719df2ea
Radomyr <qulowg@gmail.com>
2026-03-02 21:24:09 +0200

    feat: Update main.py file and add uk.json for testing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import asyncio
 
-import vesna.locale
+import vesna
 
 async def main():
-    l = vesna.locale.Locale("en")
-    print(l["test"])
+    vesna.i18n.is_exception = True
+    print(await vesna.i18n.load_translation(
+        "tests/localisation/",
+        "{locale_code}.json",
+        "uk"))
+
+    l = vesna.Locale("uk")
+    print(l["hello"])
 
 if __name__ == "__main__":
-    asyncio.run (main())
+    asyncio.run(main())
